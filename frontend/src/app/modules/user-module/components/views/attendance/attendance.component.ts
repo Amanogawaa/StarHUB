afafb79faf933a34792ad244b3e81b1620669180
月亮 <dommolino28@gmail.com>
2024-03-10 23:49:41 +0800

    finish styling

--- frontend/src/app/modules/user-module/components/views/attendance/attendance.component.ts
+++ frontend/src/app/modules/user-module/components/views/attendance/attendance.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../template/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-attendance',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './attendance.component.html',
-  styleUrl: './attendance.component.scss'
+  styleUrl: './attendance.component.css',
 })
-export class AttendanceComponent {
-
-}
+export class AttendanceComponent {}
--- frontend/src/app/modules/user-module/components/views/profile/profile.component.ts
+++ frontend/src/app/modules/user-module/components/views/profile/profile.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../template/sidebar/sidebar.component';
+import { Students } from './students';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  students: Students = {
+    name: 'Greg',
+    studentNumber: 20221234,
+    course: 'BSCS',
+    email: '20221234@gordoncollege.edu.ph',
+  };
 
+  onFileSelect(event: any) {
+    event.target.files[0];
+  }
 }
