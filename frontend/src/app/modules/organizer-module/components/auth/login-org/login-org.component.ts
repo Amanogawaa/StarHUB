0ce450814cd1ac42d9a7d068c06690320fbcbe07
月亮 <dommolino28@gmail.com>
2024-03-12 23:58:20 +0800

    add organizer login component

---
+++ frontend/src/app/modules/organizer-module/components/auth/login-org/login-org.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-org',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-org.component.html',
+  styleUrl: './login-org.component.scss'
+})
+export class LoginOrgComponent {
+
+}
