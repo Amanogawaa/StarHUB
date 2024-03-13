e8522c4431a622c7531b0bae8ed46cf859d32a22
月亮 <dommolino28@gmail.com>
2024-03-13 22:03:21 +0800

    restructure files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/user-module/components/auth/login/login.component';
-import { SignupComponent } from './modules/user-module/components/auth/signup/signup.component';
-import { DashboardComponent } from './modules/user-module/components/views/dashboard/dashboard.component';
-import { ProfileComponent } from './modules/user-module/components/views/profile/profile.component';
-import { EventsComponent } from './modules/user-module/components/views/events/events.component';
-import { AttendanceComponent } from './modules/user-module/components/views/attendance/attendance.component';
-import { FeedbackComponent } from './modules/user-module/components/views/feedback/feedback.component';
-import { EventsOrgComponent } from './modules/organizer-module/components/views/events-org/events-org.component';
-import { DashboardOrgComponent } from './modules/organizer-module/components/views/dashboard-org/dashboard-org.component';
-import { LoginOrgComponent } from './modules/organizer-module/components/auth/login-org/login-org.component';
+
+import { LoginComponent } from './modules/user/components/auth/login/login.component';
+import { SignupComponent } from './modules/user/components/auth/signup/signup.component';
+import { DashboardComponent } from './modules/user/components/views/dashboard/dashboard.component';
+import { ProfileComponent } from './modules/user/components/views/profile/profile.component';
+import { EventsComponent } from './modules/user/components/views/events/events.component';
+import { AttendanceComponent } from './modules/user/components/views/attendance/attendance.component';
+import { FeedbackComponent } from './modules/user/components/views/feedback/feedback.component';
+import { OrganizerDashboardComponent } from './modules/organizer/components/views/organizer-dashboard/organizer-dashboard.component';
+import { OrganizerLoginComponent } from './modules/organizer/components/auth/organizer-login/organizer-login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -19,7 +19,6 @@ export const routes: Routes = [
   { path: 'events', component: EventsComponent },
   { path: 'attendance', component: AttendanceComponent },
   { path: 'feedback', component: FeedbackComponent },
-  { path: 'organizer/events', component: EventsOrgComponent },
-  { path: 'organizer/dashboard', component: DashboardOrgComponent },
-  { path: 'organizer/login', component: LoginOrgComponent },
+  { path: 'organizer/dashboard', component: OrganizerDashboardComponent },
+  { path: 'organizer/login', component: OrganizerLoginComponent },
 ];
---
+++ frontend/src/app/modules/user/components/views/attendance/attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../template/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-attendance',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, SidebarComponent],
+  templateUrl: './attendance.component.html',
+  styleUrl: './attendance.component.css',
+})
+export class AttendanceComponent {}
