536545da0085e239a138a64f85a39dcd77618786
月亮 <dommolino28@gmail.com>
2024-03-19 22:11:34 +0800

    add router link

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
         white: {
           gainsboro: "#DCDCDC",
           alice_blue: "#f0f8ff",
+          bone_white: "#F9F6EE",
+        },
+        green: {
+          meduim_sea_green: "#3CB371",
+          sea_green: "#2E8B57	",
+          meduim_spring_green: "#00FA9A",
+          spring_green: "#00FF7F",
+          forest_green: "#228B22",
+          lime_green: "#32CD32",
+          lime: "#00FF00",
         },
         black: {
           midnight_blue: "#101720",
